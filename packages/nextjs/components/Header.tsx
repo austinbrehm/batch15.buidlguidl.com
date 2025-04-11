15fe873c071034d4a2818f5d86b9e855daf2f325
Smartdevs17 <smartdevs17@gmail.com>
2025-04-12 00:06:27 +0100

    Revert "feat: add batch membership and check-in status indicators to wallet button"
    This reverts commit 968de6bd84ae34699eefaeaf1af130ed020ec927.

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -5,10 +5,9 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { hardhat } from "viem/chains";
-import { useAccount } from "wagmi";
 import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
-import { useOutsideClick, useScaffoldReadContract, useTargetNetwork } from "~~/hooks/scaffold-eth";
+import { useOutsideClick, useTargetNetwork } from "~~/hooks/scaffold-eth";
 
 type HeaderMenuLink = {
   label: string;
@@ -68,25 +67,6 @@ export const Header = () => {
     useCallback(() => setIsDrawerOpen(false), []),
   );
 
-  const { address } = useAccount();
-
-  // Check if address is in allowList
-  const { data: isBatchMember } = useScaffoldReadContract({
-    contractName: "BatchRegistry",
-    functionName: "allowList",
-    args: [address],
-  });
-
-  // Check if address is checked in
-  const { data: checkedInContractAddress } = useScaffoldReadContract({
-    contractName: "BatchRegistry",
-    functionName: "yourContractAddress",
-    args: [address],
-  });
-
-  const isCheckedIn =
-    checkedInContractAddress && checkedInContractAddress !== "0x0000000000000000000000000000000000000000";
-
   return (
     <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
       <div className="navbar-start w-auto lg:w-1/2">
@@ -125,35 +105,7 @@ export const Header = () => {
           <HeaderMenuLinks />
         </ul>
       </div>
-
-      <div className="navbar-end flex-grow mr-4 flex items-center gap-2">
-        {address && (
-          <div className="flex items-center gap-1">
-            {isBatchMember && (
-              <div className="tooltip tooltip-bottom" data-tip="Batch Member">
-                <svg className="w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20">
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
-            )}
-            {isCheckedIn && (
-              <div className="tooltip tooltip-bottom" data-tip="Checked In">
-                <svg className="w-5 h-5 text-blue-500" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
-                  <path
-                    fillRule="evenodd"
-                    d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
-            )}
-          </div>
-        )}
+      <div className="navbar-end flex-grow mr-4">
         <RainbowKitCustomConnectButton />
         {isLocalNetwork && <FaucetButton />}
       </div>
