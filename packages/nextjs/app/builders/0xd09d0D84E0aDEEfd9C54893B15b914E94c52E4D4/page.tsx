39e7c7d6e739e598ea25b3afd4b5b53fe2721abc
PavelArs <pro100pavel.ars@yandex.ru>
2025-04-15 10:55:09 +0300

    fix: review comments

--- packages/nextjs/app/builders/0xd09d0D84E0aDEEfd9C54893B15b914E94c52E4D4/page.tsx
+++ packages/nextjs/app/builders/0xd09d0D84E0aDEEfd9C54893B15b914E94c52E4D4/page.tsx
@@ -1,17 +1,15 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
 import type { NextPage } from "next";
 import { FaGithub, FaLinkedin, FaTelegram, FaTwitter } from "react-icons/fa";
 import { Address } from "~~/components/scaffold-eth";
 
-const Home: NextPage = () => {
+const PavelArsProfile: NextPage = () => {
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="flex flex-row px-5 max-w-4xl mx-auto gap-8">
-          <div className="flex flex-col items-center w-1/3">
+        <div className="flex flex-col md:flex-row px-5 max-w-4xl mx-auto gap-8">
+          <div className="flex flex-col items-center w-full md:w-1/3">
             <div className="mb-6">
               <div className="avatar">
                 <div className="w-36 h-36 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 overflow-hidden">
@@ -23,9 +21,9 @@ const Home: NextPage = () => {
               <span className="block text-4xl font-bold">Pavel Ars</span>
             </h1>
             <span className="block mb-6 text-s text-center text-gray-500">
-              Mango Monster Enjoyer & Custom Keyboards Enthusiast
+              Mango Monster Enjoyer & Custom Mechanical Keyboards Enthusiast
             </span>
-            <div className="flex mb-6 gap-4 items-center">
+            <div className="flex flex-wrap mb-6 gap-4 items-center">
               <Link href="https://github.com/pavelars" passHref className="btn btn-circle btn-outline">
                 <FaGithub size={24} />
               </Link>
@@ -43,7 +41,7 @@ const Home: NextPage = () => {
               <Address address="0xd09d0D84E0aDEEfd9C54893B15b914E94c52E4D4" />
             </div>
           </div>
-          <div className="flex flex-col w-2/3">
+          <div className="flex flex-col w-full md:w-2/3 mt-8 md:mt-0">
             <h1 className="text-center mb-4">
               <span className="block text-2xl mb-2">About me</span>
             </h1>
@@ -65,4 +63,4 @@ const Home: NextPage = () => {
   );
 };
 
-export default Home;
+export default PavelArsProfile;
