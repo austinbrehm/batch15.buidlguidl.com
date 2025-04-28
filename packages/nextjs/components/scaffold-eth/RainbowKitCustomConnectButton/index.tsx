e1c6e2a894520f0ceed9d991381ee6b547e677cb
amsorrytola <talhaansariitr@gmail.com>
2025-04-28 12:46:25 +0530

    fix: package.json

--- packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton/index.tsx
+++ packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton/index.tsx
@@ -25,6 +25,7 @@ export const RainbowKitCustomConnectButton = () => {
         const blockExplorerAddressLink = account
           ? getBlockExplorerAddressLink(targetNetwork, account.address)
           : undefined;
+
         return (
           <>
             {(() => {
