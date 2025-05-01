05c998f140b30d3052de1eb2b1bbfc3aec418eb4
phipsae <philip.krause88@yahoo.de>
2025-05-01 16:52:30 -0400

    rebase main

--- packages/nextjs/components/MotionElements.tsx
+++ packages/nextjs/components/MotionElements.tsx
@@ -11,5 +11,17 @@ const MotionSpan = motion.span;
 const MotionPath = motion.path;
 const MotionLinearGradient = motion.linearGradient;
 const MotionS = motion.section;
+const MotionSection = motion.section;
 
-export { MotionA, MotionDiv, MotionH1, MotionMain, MotionP, MotionSpan, MotionPath, MotionLinearGradient, MotionS };
+export {
+  MotionA,
+  MotionDiv,
+  MotionH1,
+  MotionMain,
+  MotionP,
+  MotionSpan,
+  MotionPath,
+  MotionLinearGradient,
+  MotionS,
+  MotionSection,
+};
